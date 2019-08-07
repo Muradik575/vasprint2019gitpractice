797e8f055771d1d2024da32f02e572eb79494061
“Muradik575” <muradsalimov@gmail.com>
2019-08-06 21:29:38 -0400

    changed numbers

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,6 +2,7 @@ public class LoginTest {
     public static void main(String[] args) {
         System.out.println("Line 1");
         System.out.println("Line 2");
-        System.out.println("Line3");
+        System.out.println("Line 3");
+        System.out.println("Line 4");
     }
 }
