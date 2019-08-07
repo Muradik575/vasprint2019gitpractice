3c6c1be71a72e7d33ef63d5c352a0f2cad5ccc85
Muradik575 <50150507+Muradik575@users.noreply.github.com>
2019-08-06 20:16:50 -0400

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,7 +1,6 @@
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("yo");
+       
     }
 }
